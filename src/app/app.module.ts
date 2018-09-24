601b0539dfdf2e247a69b2033e26d06c36cb4ba2
mouradevsoltani <mouradevsoltani@gmail.com>
2018-09-24 14:39:32 +0200

    creation des modules: login, pageNotFound, prestation, shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { LoginModule } from './login/login.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiModule,
+    LoginModule,
+    PageNotFoundModule
   ],
   providers: [],
   bootstrap: [AppComponent]
