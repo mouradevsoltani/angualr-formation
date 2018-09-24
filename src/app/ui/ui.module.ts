c89a533bb076ebd4a64ebfadab181a5bb2c35368
mouradevsoltani <mouradevsoltani@gmail.com>
2018-09-24 14:54:32 +0200

    creation des components et containers

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiComponent } from './containers/ui/ui.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [UiComponent, HeaderComponent, NavComponent, FooterComponent]
 })
 export class UiModule { }
